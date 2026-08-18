cde0b014328120611719fea5074bebfdc95d076e
Sazid <mahmuduzzamansazid@gmail.com>
2026-08-18 12:24:46 +0600

    house keeping and guest management feature structure added

--- HotelManagementSystem/obj/Debug/net10.0/HotelManagementSystem.AssemblyInfo.cs
+++ HotelManagementSystem/obj/Debug/net10.0/HotelManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03acaf8c6e299dc33ea3ae72e1b33f1968a4b77d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2eab6168192763d6fabbf0b1a0c8db688cd094")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
