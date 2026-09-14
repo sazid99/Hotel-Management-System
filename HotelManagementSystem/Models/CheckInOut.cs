d340c1f5b96cb54d0df67b0613e2d979dcca7bd3
HP <jannatulferdous3727@gmail.com>
2026-09-14 21:43:07 +0600

    Check In Check out

---
+++ HotelManagementSystem/Models/CheckInOut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagementSystem.Models
+{
+    public class CheckInOut
+    {
+        [Key]
+        public int CheckInOutId { get; set; }
+
+        [Required]
+        public int ReservationId { get; set; }
+
+        [Required]
+        public DateTime CheckInDate { get; set; }
+
+        public DateTime? CheckOutDate { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
